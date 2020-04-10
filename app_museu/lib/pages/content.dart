58052c572b918cc10f7047b0a0c44fa24c0a4fe6
Emerson Costa <emerson.esn@gmail.com>
2020-04-10 13:28:31 -0300

    Feita adição de uma imagem que estava faltando e conclusão da tela 'content'

--- app_museu/lib/pages/content.dart
+++ app_museu/lib/pages/content.dart
@@ -24,22 +24,125 @@ class Content extends StatelessWidget {
                 
                 ),
         child: Scaffold(
-          
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            title: Text('My App'),
-            centerTitle: true,
-            leading: IconButton(
-                icon: Icon(
-                  Icons.list,
-                  color: Colors.white,
+             body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+            child: Column(
+              // Column is also a layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug painting" (press "p" in the console, choose the
+              // "Toggle Debug Paint" action from the Flutter Inspector in Android
+              // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+              // to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              
+              children: <Widget>[
+                  Container(
+                     color: Colors.red[50],
+                     width: 370,
+                     height: 560,
+                     padding: const EdgeInsets.all(10.0),
+                     alignment:  Alignment.topCenter,
+                     child: Stack(
+                       children: <Widget>[
+                          Container(
+                              height: 410.0,
+                              alignment:  Alignment.bottomCenter,
+                              padding: const EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                  image: AssetImage('assets/img/quadro.jpg'), 
+                                  alignment: Alignment.topCenter, 
+                                  fit: BoxFit.fitHeight   
+                                  )
+                              ),  
+                            ),
+                            Container(
+                         alignment:  Alignment.bottomCenter,
+                         child: Text("O Grito é a obra-prima do pintor norueguês Edvard Munch. Pintada pela primeira vez em 1893, a tela foi ganhando três novas versões com o passar do tempo. Historicamente a obra de Munch é classificada como iniciadora do expressionismo (um importante movimento modernista da primeira parte do século XX). As suas telas são densas e abordam temas difíceis como a solidão, a melancolia, a ansiedade e o medo.",
+                         textAlign: TextAlign.center,
+                        ), 
+                       ),
+
+
+                       ],
+                     )
+                    
+                     
+                    ),
+
+                    ],
+                  ),
                 ),
-                onPressed: () {}),
-          ),
-        ),
-      ),
+                backgroundColor: Colors.transparent,
+/*
+                bottomNavigationBar: BottomAppBar(
+                  color: Colors.yellow,
+                  child: 
+                      Container(height: 50.0,)
+                ),
+*/                
+                floatingActionButtonLocation: 
+                  FloatingActionButtonLocation.centerFloat,
+                  floatingActionButton: Stack(
+                    children: <Widget>[
+                       Align(
+                          alignment: Alignment.bottomLeft,
+                          
+                          child: FloatingActionButton(
+                            elevation: 20,
+                            backgroundColor: Color.fromRGBO(54,54,54, 1),
+                            child: Icon(
+                              Icons.home,
+                              size: 45,
+                              )
+                            ),
+                        ),
+
+                       Align(
+                          alignment: Alignment.bottomCenter,
+                          child:
+                            Container(
+                              height: 80.0,
+                              width: 80.0,
+                              
+                              child: FloatingActionButton(
+                                elevation: 20,
+                                backgroundColor: Color.fromRGBO(54, 54, 54, 100.0),
+                                child: Icon(
+                                  Icons.search,
+                                  size: 70,
+                                  
+                                  )
+                                ),
+                            
+                            ),
+                           
+                        ),
+                       
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: FloatingActionButton(
+                            elevation: 90,
+                            backgroundColor: Color.fromRGBO(54,54,54, 1),
+                            child: Icon(Icons.close,
+                                        size: 50,
+                                        )
+                            ),
+                        ),
+                      ],
+                  )
+                  ),
+                  ),
+
     );
   }
 }
